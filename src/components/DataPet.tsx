09ce42d233c4550674b6b5445e0d227815f84505
DaivyMorales <daivymorales7705@gmail.com>
2024-06-26 11:40:25 -0500

    feat: DataPet Component reutilizable

--- src/components/DataPet.tsx
+++ src/components/DataPet.tsx
@@ -1,31 +1,37 @@
-import { useState } from "react";
+import {useState} from "react";
 
 interface DataPetProps {
+  name: string;
   question: string | undefined;
-  placeholder: string | undefined;
-  inputType: string | "text" | "number" | "option" | undefined;
+  placeholder?: string;
+  inputType: string |"text" | "number" | "option" | undefined;
   option?: {
     optionOne: string;
     optionTwo: string;
   };
-  type?: string | number | boolean;
   buttonText: string;
   onButtonClick: () => void;
   measurement?: string;
+  setFieldValue: (field: string, value: any, shouldValidate?: boolean) => void;
 }
 
 function DataPet({
+  name,
   question,
   placeholder,
   inputType,
-  type,
   buttonText,
   onButtonClick,
   measurement,
   option,
+  setFieldValue,
 }: DataPetProps) {
+  const [optionSelected, setOptionSelected] = useState("");
 
-  const [optionSelected, setOptionSelected] = useState(0)
+  const handleOptionSelect = (value: string) => {
+    setFieldValue(name, value);
+    setOptionSelected(value);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center gap-3">
@@ -33,49 +39,71 @@ function DataPet({
       {inputType === "text" && (
         <div className="inputDataPet flex rounded-lg border-[1px] border-neutral-300 p-2 text-sm shadow-inner shadow-sm">
           <input
+            name={name}
             type="text"
-            className=" font-normal placeholder:text-neutral-400"
+            className="font-normal placeholder:text-neutral-400"
             placeholder={placeholder}
+            onChange={(e) => {
+              setFieldValue(name, e.target.value);
+            }}
           />
         </div>
       )}
       {inputType === "number" && (
         <div className="inputDataPet flex w-[100px] rounded-lg border-[1px] border-neutral-300 text-sm shadow-inner shadow-sm">
           <input
+            name={name}
             type="number"
             className="p-2 font-normal placeholder:text-neutral-400"
             placeholder={placeholder}
+            onChange={(e) => {
+              setFieldValue(name, parseInt(e.target.value));
+            }}
           />
           <div className="rounded-r-lg bg-neutral-100 px-2 py-2 font-medium">
             {measurement}
           </div>
         </div>
       )}
-      {inputType === "option" && (
+      {inputType === "option" && option &&(
         <div className="flex gap-3">
-          <label onClick={() => setOptionSelected(1)} className={`${optionSelected === 1 ? "text-blue-500 border-blue-300" : "text-neutral-500"} text-xs cursor-pointer rounded-lg border-[1px] p-3 font-medium`}>
+          <label
+            onClick={() => handleOptionSelect(option.optionOne)}
+            className={`${
+              optionSelected === option?.optionOne ? "border-blue-300 text-blue-500" : "text-neutral-500"
+            } cursor-pointer rounded-lg border-[1px] p-3 text-xs font-medium`}
+          >
             <input
-              className="hidden"
               type="radio"
-              name="dogGenre"
-              id={option?.optionOne}
+              name={name}
+              className="hidden"
+              value={option?.optionOne}
+              checked={optionSelected === option?.optionOne}
+              onChange={() => {}}
             />
             {option?.optionOne}
           </label>
-          <label onClick={() => setOptionSelected(2)} className={`${optionSelected === 2 ? "text-blue-500 border-blue-300" : "text-neutral-500"} text-xs cursor-pointer rounded-lg border-[1px] p-3 font-medium`}>
+          <label
+            onClick={() => handleOptionSelect(option.optionTwo)}
+            className={`${
+              optionSelected === option?.optionTwo ? "border-blue-300 text-blue-500" : "text-neutral-500"
+            } cursor-pointer rounded-lg border-[1px] p-3 text-xs font-medium`}
+          >
             <input
-              className="hidden"
               type="radio"
-              name="dogGenre"
-              id={option?.optionTwo}
+              name={name}
+              className="hidden"
+              value={option?.optionTwo}
+              checked={optionSelected === option?.optionTwo}
+              onChange={() => {}}
             />
             {option?.optionTwo}
           </label>
         </div>
       )}
       <button
-        onClick={() => onButtonClick()}
-        className="w-[180px] rounded-lg bg-blue-600 py-2  text-sm font-medium text-white shadow-inner shadow-sm"
+        onClick={onButtonClick}
+        className="w-[180px] rounded-lg bg-blue-600 py-2 text-sm font-medium text-white shadow-inner shadow-sm"
       >
         {buttonText}
       </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,66 @@
 import DataPet from "@/components/DataPet";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 import { useState } from "react";
+import { useFormik } from "formik";
 
 function index() {
   const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
 
   const [step, setStep] = useState(0);
 
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      weight: 0,
+      breed: "",
+      genre: "",
+      age: 0,
+      isVacunned: "",
+    },
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
   const questions = [
     {
+      name: "name",
       question: "¿Cuál es el nombre de tu perro?",
       placeholder: "Max",
       inputType: "text",
     },
     {
+      name: "weight",
       question: "¿Cuál es el peso de tu perro en kg?",
       placeholder: "30",
       inputType: "number",
-      measurement: "kg"
+      measurement: "kg",
     },
     {
+      name: "breed",
       question: "¿Cuál es la raza de tu perro?",
       placeholder: "Golden Retriever",
       inputType: "text",
     },
     {
+      name: "genre",
       question: "¿Qué sexo es tu perro?",
       placeholder: "Masculino",
       inputType: "option",
       option: {
         optionOne: "Masculino",
-        optionTwo: "Femenino"
-      }
+        optionTwo: "Femenino",
+      },
     },
     {
+      name: "age",
       question: "¿Cuantos años tiene tu perro?",
       placeholder: "3",
       inputType: "number",
-      measurement: "años"
+      measurement: "años",
     },
     {
+      name: "isVacunned",
       question: "¿Tu perro ha sido vacunado alguna vez?",
       placeholder: "yes",
       inputType: "text",
@@ -67,16 +88,20 @@ function index() {
         <XAxis dataKey="name" />
         <YAxis />
       </LineChart> */}
-
-      <DataPet
-        question={questions[step]?.question}
-        placeholder={questions[step]?.placeholder}
-        inputType={questions[step]?.inputType}
-        buttonText="Siguiente"
-        onButtonClick={handleNext}
-        option={questions[step]?.option}
-        measurement={questions[step]?.measurement}
-      />
+      <form onSubmit={formik.handleSubmit}>
+        <DataPet
+          name={questions[step]?.name ?? ""}
+          question={questions[step]?.question}
+          placeholder={questions[step]?.placeholder}
+          inputType={questions[step]?.inputType}
+          buttonText="Siguiente"
+          onButtonClick={handleNext}
+          option={questions[step]?.option}
+          measurement={questions[step]?.measurement}
+          setFieldValue={formik.setFieldValue}
+        />
+        {/* <button type="submit">Submit</button> */}
+      </form>
     </div>
   );
 }
