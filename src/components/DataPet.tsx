6f58ad3ec9141bb41e277bdf6d584c6c06c07433
DaivyMorales <daivymorales7705@gmail.com>
2024-06-26 09:35:17 -0500

    feat: DataPet Component accepting type text, number or option input

--- src/components/DataPet.tsx
+++ src/components/DataPet.tsx
@@ -1,9 +1,15 @@
 interface DataPetProps {
-  question: string;
-  placeholder: string;
-  inputType: "text" | "number" | "checkbox";
+  question: string | undefined;
+  placeholder: string | undefined;
+  inputType: string | "text" | "number" | "option" | undefined;
+  option?: {
+    optionOne: string ;
+    optionTwo: string ;
+  };
   type?: string | number | boolean;
   buttonText: string;
+  onButtonClick: () => void;
+  measurement?: string;
 }
 
 function DataPet({
@@ -12,27 +18,51 @@ function DataPet({
   inputType,
   type,
   buttonText,
+  onButtonClick,
+  measurement,
+  option,
 }: DataPetProps) {
   return (
     <div className="flex flex-col items-center justify-center gap-3">
       <h3>{question}</h3>
-      <div className="rounded-lg border-[1px] border-neutral-300 p-2 text-sm shadow-inner shadow-sm">
-        {inputType === "text" && (
+      {inputType === "text" && (
+        <div className="inputDataPet flex rounded-lg border-[1px] border-neutral-300 p-2 text-sm shadow-inner shadow-sm">
           <input
             type="text"
-            className="font-normal placeholder:text-neutral-400"
+            className=" font-normal placeholder:text-neutral-400"
             placeholder={placeholder}
           />
-        )}
-        {inputType === "number" && (
+        </div>
+      )}
+      {inputType === "number" && (
+        <div className="inputDataPet flex w-[100px] rounded-lg border-[1px] border-neutral-300 text-sm shadow-inner shadow-sm">
           <input
             type="number"
-            className="font-normal placeholder:text-neutral-400"
+            className="p-2 font-normal placeholder:text-neutral-400"
             placeholder={placeholder}
           />
-        )}
-      </div>
-      <button className="w-[180px] rounded-lg bg-blue-600 py-2 text-sm font-medium text-white shadow-inner shadow-sm">
+          <div className="rounded-r-lg bg-neutral-100 px-2 py-2 font-medium">
+            {measurement}
+          </div>
+        </div>
+      )}
+      {inputType === "option" && (
+        <div className="flex gap-3">
+         <label className="border-[1px] rounded-lg p-3 cursor-pointer font-medium text-neutral-500">
+          <input className="hidden" type="radio" name="dogGenre" id={option?.optionOne} />
+          {option?.optionOne}
+         </label>
+         <label className="border-[1px] rounded-lg p-3 cursor-pointer font-medium text-neutral-500">
+          <input className="hidden" type="radio" name="dogGenre" id={option?.optionTwo} />
+          {option?.optionTwo}
+         </label>
+          
+        </div>
+      )}
+      <button
+        onClick={() => onButtonClick()}
+        className="w-[180px] rounded-lg bg-blue-600 py-2  text-sm font-medium text-white shadow-inner shadow-sm"
+      >
         {buttonText}
       </button>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,58 @@
 import DataPet from "@/components/DataPet";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+import { useState } from "react";
 
 function index() {
   const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
+
+  const [step, setStep] = useState(0);
+
+  const questions = [
+    {
+      question: "¿Cuál es el nombre de tu perro?",
+      placeholder: "Max",
+      inputType: "text",
+    },
+    {
+      question: "¿Cuál es el peso de tu perro en kg?",
+      placeholder: "30",
+      inputType: "number",
+      measurement: "kg"
+    },
+    {
+      question: "¿Cuál es la raza de tu perro?",
+      placeholder: "Golden Retriever",
+      inputType: "text",
+    },
+    {
+      question: "¿Qué sexo es tu perro?",
+      placeholder: "Masculino",
+      inputType: "option",
+      option: {
+        optionOne: "Masculino",
+        optionTwo: "Femenino"
+      }
+    },
+    {
+      question: "¿Cuantos años tiene tu perro?",
+      placeholder: "3",
+      inputType: "number",
+      measurement: "años"
+    },
+    {
+      question: "¿Tu perro ha sido vacunado alguna vez?",
+      placeholder: "yes",
+      inputType: "text",
+    },
+  ];
+
+  const handleNext = () => {
+    if (step < questions.length - 1) {
+      setStep(step + 1);
+    } else {
+      console.log("All question answered");
+    }
+  };
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-3">
@@ -19,45 +69,13 @@ function index() {
       </LineChart> */}
 
       <DataPet
-        question="¿Cuál es el nombre de tu perro?"
-        placeholder="Max"
-        inputType="text"
+        question={questions[step]?.question}
+        placeholder={questions[step]?.placeholder}
+        inputType={questions[step]?.inputType}
         buttonText="Siguiente"
-      />
-
-      <DataPet
-        question="¿Cuál es el peso de tu perro en kg?"
-        placeholder="30"
-        inputType="number"
-        buttonText="Siguiente"
-      />
-
-      <DataPet
-        question="¿Cuál es la raza de tu perro?"
-        placeholder="Golden Retriever"
-        inputType="text"
-        buttonText="Siguiente"
-      />
-
-      <DataPet
-        question="¿Qué sexo es tu perro?"
-        placeholder="Masculino"
-        inputType="text"
-        buttonText="Siguiente"
-      />
-
-      <DataPet
-        question="¿Cuantos años tiene tu perro?"
-        placeholder="8 years old"
-        inputType="number"
-        buttonText="Siguiente"
-      />
-
-      <DataPet
-        question="¿Tu perro ha sido vacunado alguna vez?"
-        placeholder="8 years old"
-        inputType="text"
-        buttonText="Siguiente"
+        onButtonClick={handleNext}
+        option={questions[step]?.option}
+        measurement={questions[step]?.measurement}
       />
     </div>
   );
